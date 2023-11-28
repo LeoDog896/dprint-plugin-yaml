a846badce12adff0a5e29724c70a8fc9777ea910
Tristan F. <LeoDog896@hotmail.com>
2023-11-28 18:53:28 +0000

    feat: use yaml and yml file extensions

--- src/lib.rs
+++ src/lib.rs
@@ -4,67 +4,82 @@ use dprint_core::configuration::get_value;
 use dprint_core::configuration::ConfigKeyMap;
 use dprint_core::configuration::GlobalConfiguration;
 use dprint_core::configuration::ResolveConfigurationResult;
+use dprint_core::plugins::FileMatchingInfo;
 use dprint_core::plugins::PluginInfo;
 use dprint_core::plugins::SyncPluginHandler;
-use std::path::Path;
+use dprint_core::plugins::SyncPluginInfo;
 use serde::Serialize;
+use std::path::Path;
 
 #[derive(Clone, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Configuration {
-  // add configuration properties here...
-  line_width: u32, // for example
+    // add configuration properties here...
+    line_width: u32, // for example
 }
 
 pub struct MyPluginHandler {}
 
 impl MyPluginHandler {
-  const fn new() -> Self {
-    MyPluginHandler {}
-  }
+    const fn new() -> Self {
+        MyPluginHandler {}
+    }
 }
 
 impl SyncPluginHandler<Configuration> for MyPluginHandler {
-  fn plugin_info(&mut self) -> PluginInfo {
-    PluginInfo {
-      name: env!("CARGO_PKG_NAME").to_string(),
-      version: env!("CARGO_PKG_VERSION").to_string(),
-      config_key: "keyGoesHere".to_string(),
-      file_extensions: vec!["txt_ps".to_string()],
-      file_names: vec![],
-      help_url: "".to_string(),          // fill this in
-      config_schema_url: "".to_string(), // leave this empty for now
-      update_url: None,                  // leave this empty for now
+    fn plugin_info(&mut self) -> SyncPluginInfo {
+        SyncPluginInfo {
+            info: PluginInfo {
+                name: env!("CARGO_PKG_NAME").to_string(),
+                version: env!("CARGO_PKG_VERSION").to_string(),
+                config_key: "yaml".to_string(),
+                help_url: "https://github.com/LeoDog896/dprint-plugin-yaml/issues".to_string(),
+                config_schema_url: "".to_string(), // leave this empty for now
+                update_url: None,                  // leave this empty for now
+            },
+            file_matching: FileMatchingInfo {
+                file_extensions: vec![".yaml".to_string(), ".yml".to_string()],
+                file_names: vec![]
+            }
+        }
     }
-  }
 
-  fn license_text(&mut self) -> String {
-    include_str!("../LICENSE").to_string()
-  }
-
-  fn resolve_config(&mut self, config: ConfigKeyMap, global_config: &GlobalConfiguration) -> ResolveConfigurationResult<Configuration> {
-    // implement this... for example
-    let mut config = config;
-    let mut diagnostics = Vec::new();
-    let line_width = get_value(&mut config, "line_width", global_config.line_width.unwrap_or(120), &mut diagnostics);
-
-    diagnostics.extend(get_unknown_property_diagnostics(config));
-
-    ResolveConfigurationResult {
-      config: Configuration { line_width },
-      diagnostics,
+    fn license_text(&mut self) -> String {
+        include_str!("../LICENSE").to_string()
     }
-  }
 
-  fn format(
-    &mut self,
-    file_path: &Path,
-    file_text: Vec<u8>,
-    config: &Configuration,
-    mut format_with_host: impl FnMut(&Path, String, &ConfigKeyMap) -> Result<Option<String>>,
-  ) -> Result<Option<Vec<u8>>> {
-    // format here
-  }
+    fn resolve_config(
+        &mut self,
+        config: ConfigKeyMap,
+        global_config: &GlobalConfiguration,
+    ) -> ResolveConfigurationResult<Configuration> {
+        // implement this... for example
+        let mut config = config;
+        let mut diagnostics = Vec::new();
+        let line_width = get_value(
+            &mut config,
+            "line_width",
+            global_config.line_width.unwrap_or(120),
+            &mut diagnostics,
+        );
+
+        diagnostics.extend(get_unknown_property_diagnostics(config));
+
+        ResolveConfigurationResult {
+            config: Configuration { line_width },
+            diagnostics,
+        }
+    }
+
+    fn format(
+        &mut self,
+        file_path: &Path,
+        file_text: Vec<u8>,
+        config: &Configuration,
+        mut format_with_host: impl FnMut(&Path, String, &ConfigKeyMap) -> Result<Option<Vec<u8>>>,
+    ) -> Result<Option<Vec<u8>>> {
+        // format here
+    }
 }
 
 generate_plugin_code!(MyPluginHandler, MyPluginHandler::new());
